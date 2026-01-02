5e70c55979f949db27f4e9d2b021d4a818cc2f19
jjmercado <jjmercado@users.noreply.github.com>
2026-01-02 15:03:57 +0000

    deploy: 77d1a71f61764033e4917a71c1437c255bde90ba

---
+++ _nuxt/AOLv4Sce.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./cPm-f_bN.js";import{v as n,o as s,w as f,O as p}from"./TsqhPJvg.js";const l={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const o=a;return s(),n(o,{href:t.href,target:t.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}};export{l as default};
