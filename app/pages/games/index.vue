a40f5e677f9e10db7318819716196f8a3ac88e9c
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2026-01-05 19:22:07 +0100

    fix: add baseURL

--- app/pages/games/index.vue
+++ app/pages/games/index.vue
@@ -14,7 +14,7 @@
           <!-- Standbild (wird angezeigt, wenn nicht gehovert) -->
           <img :src="'/portfolioSite' + game.image" :alt="`${game.title} Standbild`" class="w-full h-full object-cover block group-hover:hidden group-focus-within:hidden">
           <!-- Animiertes GIF (wird bei Hover angezeigt) -->
-          <img :src="game.video" :alt="`${game.title} Animation`" class="w-full h-full object-cover hidden group-hover:block group-focus-within:block">
+          <img :src="'/portfolioSite' + game.video" :alt="`${game.title} Animation`" class="w-full h-full object-cover hidden group-hover:block group-focus-within:block">
         </div>
         
         <!-- Inhalts-Container mit Link zur Detailseite -->
