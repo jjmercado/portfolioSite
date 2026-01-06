2c0ce2830ce33c263c63e720eb5ca5725e4afa8a
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2026-01-06 22:09:37 +0100

    add: style and format for button

--- app/pages/games/[slug].vue
+++ app/pages/games/[slug].vue
@@ -38,7 +38,9 @@
           </div>
         </div>
         
-        <NuxtLink to="/games" class="block text-center mt-8">Zurück zur Spieleübersicht</NuxtLink>
+        <NuxtLink to="/games" class="block w-fit mx-auto mt-12 rounded-md bg-purple-600 px-4 py-3 text-base font-semibold text-white shadow-lg shadow-purple-600/20 transition-all duration-300 hover:bg-purple-500 hover:scale-105 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600">
+          Zurück zur Spieleübersicht
+        </NuxtLink>
 
       </article>
     </div>
